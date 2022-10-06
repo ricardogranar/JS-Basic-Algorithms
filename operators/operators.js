910fda96c7ebb528242b082e547c2256763fe41c
ricardograna <ricardogranarosales@gmail.com>
2022-10-06 23:06:51 +0200

    feat. operators

---
+++ operators/operators.js
@@ -0,0 +1,34 @@
+
+// 1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
+l
+let numA = 10;
+numA *= 5;
+
+alert(numA);
+
+
+// 1.2 Divide 10 por 2 y muestra el resultado en un alert.
+
+let numB = 10;
+numB /= 2;
+alert(numB);
+
+// 1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+
+alert(15 % 9); 
+
+// 1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+// teniendo dos variables y = 10 y z = 5.
+
+let y = 10;
+let z = 5;
+let x = y + z;
+alert(x);
+
+// 1.5 Usa el correcto operador de asignación que resultará en x = 50,
+// teniendo dos variables y = 10 y z = 5.
+
+y = 10;
+z = 5;
+x = y * z;
+alert(x);
